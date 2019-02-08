68fc1049508a8c11977318d56d143e0d2366eaeb
Marcel Dao <marcelnamdao@gmail.com>
2019-02-07 17:52:03 -0800

    first commit

---
+++ AssemblyExer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AssemblyExer
+{
+    class Program
+    {
+        class Wall
+        {
+
+        }
+        class Door
+        {
+
+        }
+        class Walkable
+        {
+
+        }
+        class MapData
+        {
+            int blockCount = 0;
+            int[] roomsize;
+        }
+        static void Main(string[] args)
+        {
+            //This first section cache's the various strings we'll be using
+            //this helps demonstrate that the classes & methods can all
+            //be entirely data driven.
+            string workingDir = System.IO.Directory.GetCurrentDirectory();
+            string dllFileName = "//netstandard2.0/ExternalLib.dll";
+            string extClass = "ExternalLib.MyExternalClass";
+            string extMethod = "LoadFile";
+
+            //First load the external assembly
+            Assembly ass = Assembly.LoadFile(workingDir + dllFileName);
+
+            //Get th type of the desired class (including the namespace)
+            Type typ = ass.GetType(extClass);
+
+            //Get the supporting method info so we can execute a method.
+            MethodInfo meth = typ.GetMethod(extMethod);
+
+            //Create an instance of the desired object
+            object obj = Activator.CreateInstance(typ);
+
+            //And invoke the method
+            meth.Invoke(obj, null);
+            Console.ReadLine();//Wait for random keyboard input & CR
+        }
+    }
+}
---
+++ ExternalLib/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExternalLib
+{
+    public class MyExternalClass
+    {
+        public static string LoadFile()
+        {
+            string path = "room";
+            path += ".txt";
+
+            //using (FileStream file = File.OpenRead(path))
+            //{
+            //    byte[] b = new byte[1024];
+            //    UTF8Encoding temp = new UTF8Encoding(true);
+
+            //    while (file.Read(b,0,b.Length) > 0)
+            //    {
+            //        Console.WriteLine(temp.GetString(b));
+
+            //    }
+            //}
+            StreamReader reader = File.OpenText(path);
+            string line;
+            string definitions = @"\b[\D+]\b([0-9]?,?[0-9]+)";
+            Regex defRgx = new Regex(definitions);
+
+            List<int> map = new List<int>();
+            int[,] maps = new int[0, 0];
+
+            string match = "";
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (!line.StartsWith("##"))
+                {
+                    if (line.StartsWith("[misc]"))
+                    {
+                        line = reader.ReadLine();
+
+                        while (!line.StartsWith("[end]"))
+                        {
+                            char[] delimiter = { ',', '=' };
+
+                            string result = defRgx.Match(line).ToString();
+                            string[] results = result.Split(delimiter);
+
+                            Console.WriteLine(String.Join("", results));
+                            line = reader.ReadLine();
+                        }
+                    }
+                    if (line.StartsWith("[block definitions"))
+                    {
+                        line = reader.ReadLine();
+
+                        while (!line.StartsWith("[end]"))
+                        {
+                            char[] delimiter = { ',', '=' };
+
+                            string result = defRgx.Match(line).ToString();
+                            string[] results = result.Split(delimiter);
+
+                            Console.WriteLine(String.Join("", results));
+                            line = reader.ReadLine();
+                        }
+                    }
+                    if (line.StartsWith("[room definitions]"))
+                    {
+                        line = reader.ReadLine();
+
+                        while (!line.StartsWith("[end]"))
+                        {
+
+                            string result = line;
+
+                            for (int i = 0; i < line.Length; ++i)
+                            {
+                                map.Add(line[i]);
+                            }
+
+                            Console.WriteLine(result);
+                            line = reader.ReadLine();
+                        }
+                    }
+                }
+            }
+            return match;
+        }
+    }
+}
